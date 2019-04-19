0e77cb343c2c096da68df7ac4214695c5a7b2ee1
Nicholas Spencer <nick.switch@gmail.com>
2019-04-19 00:25:51 -0500

    Update scrapper

--- bin/scrape_type.sh
+++ bin/scrape_type.sh
@@ -1,45 +1,4 @@
 #!/usr/bin/env bash
 
-scrape_type() {
-    curl https://www.etsy.com/developers/documentation/reference/$1 | pup 'table[id^="resource_fields"] tr:not(:first-child) json{}' | jq '.[] | {"property": .children[0].children[0].text, "type": .children[3].children[0].children[0].text, "secondaryType": (.children[3].children[0].text | try gsub("[\\(\\) ]"; "") catch null) }'
-}
-
-scrape_type $1
+curl https://www.etsy.com/developers/documentation/reference/$1 | pup 'table[id^="resource_fields"] tr:not(:first-child) json{}' | jq '.[] | { "property": .children[0].children[0].text, "typeInformation":(.children[3].children[0].children[0].children[0].text? // .children[3].children[0].children[0].text?), "secondaryInformation": (.children[3].children[0].text | try gsub("[\\(\\) ]"; "") catch null)}' | jq -s
 exit 0
-
-# Produces:
-# {
-#   "property": "name",
-#   "type": "string",
-#   "secondaryType": null
-# }
-# {
-#   "property": "uri",
-#   "type": "string",
-#   "secondaryType": null
-# }
-# {
-#   "property": "params",
-#   "type": null,
-#   "secondaryType": null
-# }
-# {
-#   "property": "defaults",
-#   "type": null,
-#   "secondaryType": null
-# }
-# {
-#   "property": "type",
-#   "type": "string",
-#   "secondaryType": null
-# }
-# {
-#   "property": "visibility",
-#   "type": "string",
-#   "secondaryType": null
-# }
-# {
-#   "property": "http_method",
-#   "type": "string",
-#   "secondaryType": null
-# }
