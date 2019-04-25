d1bcfdca0c365dd433d1bc2fa71f0e7972980b38
Nicholas Spencer <nick.switch@gmail.com>
2019-04-25 16:01:14 -0500

    Unedit StencilSwiftKit

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .package(url: "https://github.com/nicklockwood/SwiftFormat.git", from: "0.40.8"),
         .package(url: "https://github.com/Realm/SwiftLint.git", from: "0.31.0"),
         .package(url: "https://github.com/stencilproject/Stencil.git", from: "0.13.1"),
-        .package(url: "https://github.com/nicholasspencer/StencilSwiftKit.git", .branch("master")),
+        .package(url: "https://github.com/nicholasspencer/StencilSwiftKit.git", .branch("ExtensionScope")),
         .package(url: "https://github.com/SwiftGen/SwiftGen.git", from: "6.1.0"),
     ],
     targets: [
