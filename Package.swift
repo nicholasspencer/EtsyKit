91e3a1dfb02850c97931a00ed470d7b03ee12859
Nicholas Spencer <nick.switch@gmail.com>
2019-05-30 19:12:46 -0500

    Remove deps

--- Package.swift
+++ Package.swift
@@ -9,12 +9,12 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(name: "EtsySwift", targets: ["EtsySwift"]),
         .library(name: "EtsyTypeKit", targets: ["EtsyTypeKit"]),
-        .library(name: "EtsyStencilKit", targets: ["EtsyStencilKit"]),
+        // .library(name: "EtsyStencilKit", targets: ["EtsyStencilKit"]),
     ],
     dependencies: [
         // Dev deps
-        .package(url: "https://github.com/nicklockwood/SwiftFormat.git", from: "0.40.8"),
-        .package(url: "https://github.com/Realm/SwiftLint.git", from: "0.31.0"),
+        // .package(url: "https://github.com/nicklockwood/SwiftFormat.git", from: "0.40.8"),
+        // .package(url: "https://github.com/Realm/SwiftLint.git", from: "0.31.0"),
         .package(url: "https://github.com/stencilproject/Stencil.git", from: "0.13.1"),
         .package(url: "https://github.com/nicholasspencer/StencilSwiftKit.git", .branch("ExtensionScope")),
         .package(url: "https://github.com/SwiftGen/SwiftGen.git", from: "6.1.0"),
@@ -26,13 +26,13 @@ let package = Package(
             "EtsyTypeKit",
         ]),
         .target(name: "EtsyTypeKit", dependencies: [
-            "EtsyStencilKit",
+            // "EtsyStencilKit",
         ]),
-        .target(name: "EtsyStencilKit", dependencies: [
-            "Stencil",
-            "StencilSwiftKit",
-            "SwiftGenKit",
-        ]),
+        // .target(name: "EtsyStencilKit", dependencies: [
+        //     "Stencil",
+        //     "StencilSwiftKit",
+        //     "SwiftGenKit",
+        // ]),
         .testTarget(name: "EtsySwiftTests", dependencies: ["EtsySwift"]),
     ]
 )
